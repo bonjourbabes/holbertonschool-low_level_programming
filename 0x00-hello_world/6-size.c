cda660da8e5926fe4053681b357cd34049304c12
bonjourbabes <ikbenlarry@gmail.com>
2021-09-27 11:40:03 -0700

    edit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-/**                                                                                                         
-* main - printf prints "with proper grammar"                                                                
-* prints - print                                                                                            
-* Return: brings back zero                                                                                  
-*/
+/**
+ *
+ */
 int intVar;
 float floatVar;
 char charVar;
 long int longVar;
- long long int longLongVar;
-printf("Size of a char: %zu byte(s)\n", sizeof(charVar));
-printf("Size of an int: %zu byte(s)\n", sizeof(intVar));
-printf("Size of a long int: %zu byte(s)\n", sizeof(longVar));
-printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongVar));
-printf("Size of a float int: %zu byte(s)\n", sizeof(floatVar));
+long long int longLongVar;
+printf("Size of a char: %d byte(s)\n", sizeof(charVar));
+printf("Size of an int: %d byte(s)\n", sizeof(intVar));
+printf("Size of a long int: %d byte(s)\n", sizeof(longVar));
+printf("Size of a long long int: %d byte(s)\n", sizeof(longLongVar));
+printf("Size of a float int: %f byte(s)\n", sizeof(floatVar));
     return 0;
 }
