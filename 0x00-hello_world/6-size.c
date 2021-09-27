bd8322c0b97ff646ee5e41290947c3d6f0eec923
bonjourbabes <ikbenlarry@gmail.com>
2021-09-27 11:27:31 -0700

    edit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ float floatVar;
 char charVar;
 long int longVar;
  long long int longLongVar;
-printf("Size of a  char: %zu byte(s)\n", sizeof(charVar));
+printf("Size of a char: %zu byte(s)\n", sizeof(charVar));
 printf("Size of an int: %zu byte(s)\n", sizeof(intVar));
-printf("Size of a  long int: %zu byte(s)\n", sizeof(longVar));
-printf("Size of a  long long int: %zu byte(s)\n", sizeof(longLongVar));
-printf("Size of a  float int: %zu byte(s)\n", sizeof(floatVar));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longVar));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongVar));
+printf("Size of a float int: %zu byte(s)\n", sizeof(floatVar));
     return 0;
 }
