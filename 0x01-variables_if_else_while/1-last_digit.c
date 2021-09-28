726a9a5dfccaa16bc2df19bd1dc01e5a01be647e
bonjourbabes <ikbenlarry@gmail.com>
2021-09-28 14:24:42 -0700

    edit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,6 +12,16 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
  
  if(n % 10 > 5)
-   printf("Last digit of  ")
+   {
+   printf("Last digit of n ");
+   }
+ else if (n % 10 == 0)
+   {
+     printf("%i is", n);
+   }
+ else if (n % 10 < n)
+   {
+     printf("")
+   }
   return (0);
 }  
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,6 +5,10 @@
 */
 int main(void)
 {
-  char c;
-  for (c = 'a' ; c <= '
+char c;
+for (c = 'a' ; c <= 'd' ; c++)
+putchar(c);
+for (c = 'f'; c <= 'z' ; c++)
+putchar(c);
+return (0);
 }
