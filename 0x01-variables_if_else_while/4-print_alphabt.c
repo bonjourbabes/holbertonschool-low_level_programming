81855ee3580c5556cbe6b4f8552885d559f7e107
bonjourbabes <ikbenlarry@gmail.com>
2021-09-28 12:47:34 -0700

    edit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,3 +1,10 @@
 #include <stdio.h>
 /**
- *
+*main - uses putchar
+*Return: return value 0
+*/
+int main(void)
+{
+  char c;
+  for (c = 'a' ; c <= '
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - prints off
+*Return: return value
+*/
+int main(void)
+{
+int x;
+for (x = 0; x <= 9; x++)
+printf("%d", x);
+printf("\n");
+return (0);
+}
