041ee00b4a8d2b4d2557d8361113ebe633143ccf
bonjourbabes <ikbenlarry@gmail.com>
2021-09-28 12:25:15 -0700

    edit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main- uses printf to print 0-10
+ *Return: return value be 0
+ */
+int main(void)
+{
+int x;
+for (x = 0; x <= 10; x++)
+printf("%d\n", x);
+return (0);
+}
