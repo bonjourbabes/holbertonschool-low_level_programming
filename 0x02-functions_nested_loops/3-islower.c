ac2483951629ec7add10b7a1aa7eef6f8a8525db
bonjourbabes <ikbenlarry@gmail.com>
2021-09-29 10:25:32 -0700

    edit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
- *
- *_islower - function that lowers
- *Return: return zero success
- */
+*_islower - function that lowers
+*@c: the variable
+*Return: return zero success
+*/
 int _islower(int c)
 {
-if(c >= 'A' && ch <= 'Z' )
-{
-_putchar('1');
-}
-else
+if (c >= 'A' && c <= 'Z')
 {
 _putchar('0');
 }
+else
+{
+_putchar('1');
+}
+return (0);
 }
