1de7cad400fa10b3136864dabfa3dcf3115d193e
bonjourbabes <ikbenlarry@gmail.com>
2021-09-29 08:18:14 -0700

    edit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,13 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
 #include "main.h"
 /**
 *print_alphabet - Uses a function to print off alphabet
 *
 *Return: Return value success
 */
-char print_alphabet(void)
+void print_alphabet(void)
 {
 char c;
 for (c = 'a'; c <= 'z'; c++)
-putchar(c);
-putchar('\n');
+_putchar(c);
+_putchar('\n');
 }
