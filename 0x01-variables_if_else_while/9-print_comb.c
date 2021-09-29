d837549978732d7736ff51a9b5776f2529b79fc9
bonjourbabes <ikbenlarry@gmail.com>
2021-09-28 17:06:52 -0700

    edit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Uses putchar for
+*Return: Return value success
+*/
+int main(void)
+{
+int x = '0';
+while (x <= '9')
+{
+putchar(x);
+if (x != '9')
+{
+putchar(','); 
+putchar(' ');
+}
+x++;
+}
+putchar('\n');
+return (0);
+}
