e648055532d3337ee07204db22cc5e49bd392cba
bonjourbabes <ikbenlarry@gmail.com>
2021-09-29 08:56:08 -0700

    edit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - Uses the code.
+*
+* Return: Always 0.
+*/
+void print_alphabet_x10(void)
+{
+int counter = 0;
+while (counter <= 10)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+return (0);
+}
