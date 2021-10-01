61920ff5b0b2650c16f0f30c75f6759e841446d6
bonjourbabes <ikbenlarry@gmail.com>
2021-10-01 12:07:28 -0700

    edit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*_isupper - function makes
+*@c: variable
+*Return: Always 0.
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
