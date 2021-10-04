3611a97544fc27ec1927fe9513f2046f571769d6
bonjourbabes <ikbenlarry@gmail.com>
2021-10-04 12:07:43 -0700

    edit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *more_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+int x, y;
+for (y = 0; y <= 10; y++)
+{
+for (x = 0; x <= 14; x++)
+{
+if (x >= 10)
+{
+_putchar((x / 10) + '0');
+}
+if (x <= 14)
+{
+_putchar((x % 10) + '0');
+}
+}
+_putchar('\n');
+}
+return;
+}
