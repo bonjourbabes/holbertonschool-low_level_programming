ab9f92cf2df52341bd9244775b79f768c910f0ab
bonjourbabes <ikbenlarry@gmail.com>
2021-10-05 13:03:24 -0700

    edit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_rev - check the code
+ *@s: variable
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+
+while (*s != '\0')
+{
+s++;
+}
+ while (*s != '\0')
+   {
+     _putchar(*s);
+     s--;
+   }
+ _putchar('\n');
+}
