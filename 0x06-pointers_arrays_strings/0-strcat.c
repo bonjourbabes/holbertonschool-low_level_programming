27bfeda191839538fd9aba5e9a8bdcfb5dbc4c43
bonjourbabes <ikbenlarry@gmail.com>
2021-10-08 08:58:55 -0700

    edit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,21 @@
 #include "holberton.h"
 #include <stdio.h>
+#include "2-strlen.c"
 /**
- * main - check the code for Holberton School students.
- *
+ *_strcat - check the code for Holberton School students.
+ *@dest: variable
+ *@src: variable
  * Return: Always 0.
  */
-int main(void)
+char *_strcat(char *dest, char *src)
 {
+int i = _strlen(dest), j = 0;
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
+}
