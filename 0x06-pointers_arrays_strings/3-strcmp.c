80541dbd1786f6b79a56079477e05452b1910dc4
bonjourbabes <ikbenlarry@gmail.com>
2021-10-09 12:09:16 -0700

    edit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,12 +1,16 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  *_strcmp - check the code for Holberton School students.
- *
+ *@s1: variable
+ *@s2: variable
  * Return: Always 0.
  */
 int _strcmp(char *s1, char *s2)
 {
-
-
+while(*s1 != '\0' && *s2 != '\0' && *s1 - *s2 == 0)
+{
+s1++;
+s2++;
+}
+return *s1 - *s2;
 }
