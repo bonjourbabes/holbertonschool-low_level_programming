bad2f90b624e3759150e95ed59a0da336f76b91b
bonjourbabes <ikbenlarry@gmail.com>
2021-10-12 12:36:09 -0700

    edit

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,10 +6,13 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		s++;
-		_print_rev_recursion(s);
-		_putchar(*s);
+	 return;
+	}
+	else
+	{
+	_print_rev_recursion(s+1);
+	_putchar(*s);
 	}
 }
