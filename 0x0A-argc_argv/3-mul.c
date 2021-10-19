a7e02621d337324bb4ff81052d00f31d20c8dc28
bonjourbabes <ikbenlarry@gmail.com>
2021-10-19 07:36:56 -0700

    edit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,6 +9,7 @@
 int main(int argc, char *argv[])
 {
 	int sum;
+	
 	if (argc != 3)
 	{
 		printf("Error\n");
@@ -18,7 +19,7 @@ int main(int argc, char *argv[])
 	{
 		sum = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", sum);
-		return(0);
+		return (0);
 	}
 	return (0);
 }
