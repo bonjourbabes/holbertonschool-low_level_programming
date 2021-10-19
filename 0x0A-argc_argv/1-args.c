868058734b20c6f445f4be60f3afd51c5ed9af81
bonjourbabes <ikbenlarry@gmail.com>
2021-10-18 18:40:31 -0700

    edit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: variable
+ * @argv: variable
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc)
+		printf("%d\n", argc - 1);
+	return (0);
+}
