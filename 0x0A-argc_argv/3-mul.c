6d9be6cbf4e5057f69e52617f6356f91694ea6b8
bonjourbabes <ikbenlarry@gmail.com>
2021-10-18 18:58:11 -0700

    edit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: variable
+ * @argv: variable
+ * Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	int sum;
+	if (argc == 3)
+	{
+	sum = 
+	return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: variavle
+ * @argv: variable
+ * Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	return (0);
+}
