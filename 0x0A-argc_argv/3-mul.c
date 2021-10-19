b7df25c4d46189247e25c695d11de232e2acddca
bonjourbabes <ikbenlarry@gmail.com>
2021-10-18 19:01:32 -0700

    edit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,6 +10,13 @@ int main(int argc, char *argv[])
 	int sum;
 	if (argc == 3)
 	{
-	sum = 
+		sum = argc(argv[1]) * argc(argv[2]);
+		printf("%d\n", sum);
 	return (0);
+}
+	else
+	{
+		printf("Error");
+		return (-1);
+	}
 }
