aa4f63be5036c293c8f6d727322ec8690364f5d3
bonjourbabes <ikbenlarry@gmail.com>
2021-10-19 08:59:34 -0700

    edit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
 /**
  * main - function
  * @argc: variavle
@@ -7,5 +9,21 @@
  */
 int main(int argc, char *argv[])
 {
+	int sum = 0, i, j;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
 	return (0);
 }
