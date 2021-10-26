5f3457100aac1931e64cb363004e2a2786d3cfb3
bonjourbabes <ikbenlarry@gmail.com>
2021-10-25 17:33:27 -0700

    added null

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ *_calloc - function that allocs memory
+ *@nmemb: variable of alloc size
+ *size: variable 
+ *Return: zero
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+
+}
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ *_calloc - function that allocs memory for array
+ *@nmemb: elements
+ *@size: variable of bytes
+ *Return: zero
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i;
+	int *ptr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	
+}
