61fbd4a8d429383a896cef7ebe74584cdddc77a9
bonjourbabes <ikbenlarry@gmail.com>
2021-10-29 13:21:29 -0700

    edit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
-
 /**
- * sum_them_all - check the code
- *
+ * sum_them_all - sum function adding args
+ *@n : variables added together
  * Return: Always 0.
  */
 int sum_them_all(const unsigned int n, ...)
