f9092e2ec23936e59de2db622b9a758401f7f6b9
ikbenlarry@gmail.com <ikbenlarry@gmail.com>
2021-11-08 15:12:13 -0800

    edit

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,11 +4,24 @@
 #include "lists.h"
 
 /**
- * main - check the code
- *
+ * list_len - check the code
+ *@h: variable
  * Return: Always 0.
  */
 size_t list_len(const list_t *h)
 {
+	const list_t *new = h;
+	int nodeCount = 0;
 
+	if (h == NULL)
+	{
+		return (nodeCount);
+	}
+
+	while (new)
+	{
+		new = new->next;
+		nodeCount++;
+	}
+	return (nodeCount);
 }
