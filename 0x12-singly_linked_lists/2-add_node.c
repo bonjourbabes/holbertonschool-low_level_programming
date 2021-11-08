13a48307d7ecc3e8d62666e8e537701353ca63f9
ikbenlarry@gmail.com <ikbenlarry@gmail.com>
2021-11-08 15:40:21 -0800

    edit

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+list_t *add_node(list_t **head, const char *str){
+
+	list_t *newNode;
+	newNode = malloc(sizeof(list_t));
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+
+	newNode->str = strdup(str);
+	newNode->len = strlen(str);
+	newNode->next = *head;
+	*head = newNode;
+	return (newNode);
+}
